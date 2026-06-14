f3993d635a5182ece144041d07d3e0bfa071a260
Latihas <lyjp2002@163.com>
2026-06-15 02:44:19 +0800

    7.51.1.5

--- Unscrambler/OpcodeUtility.cs
+++ Unscrambler/OpcodeUtility.cs
@@ -26,8 +26,7 @@ public static class OpcodeUtility {
 	/// <param name="packet">The packet buffer, starting at the IPC header.</param>
 	/// <returns>The opcode of the packet.</returns>
 	public static ushort GetOpcodeFromPacketAtIpcStart(Span<byte> packet) {
-		const int start = OpcodeOffset;
-		const int end = start + 2;
-		return BitConverter.ToUInt16(packet[start..end]);
+		const int end = OpcodeOffset + 2;
+		return BitConverter.ToUInt16(packet[OpcodeOffset..end]);
 	}
 }
